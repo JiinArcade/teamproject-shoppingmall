39ba7decd167ad2da35fd4eaf9e8e5768499068c
jiin <jn_ll@naver.com>
2023-04-21 15:59:24 +0900

    header 구현

--- matter/src/components/Header.jsx
+++ matter/src/components/Header.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import 'Header.css'
+import Banner from './Banner';
+import './Header.css'
+import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
+    const loginList = ['로그인', '회원가입'];
+
   return (
-    <div>
-      
-    </div>
+      <div>
+        <Banner></Banner>
+          <div className='Header-container'>
+              <div className='logo'>
+                <img src="assets/Img/matter.logo.png" alt="matter 로고" />
+              </div>
+
+              <div className='userList'>
+                  <li>로그인</li>
+                  <li>회원가입</li>
+                  <li className='icon icon-heart'>
+                      <FontAwesomeIcon icon={faHeart} />
+                  </li>
+                  <li className='icon icon-cart'>
+                  <FontAwesomeIcon icon={faCartShopping} />
+                  </li>
+              </div>
+          </div>
+      </div>
   )
 }
 
