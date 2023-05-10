a82084209f323a1de85d76c7c8122fa0c1e9f4e8
jiin <jn_ll@naver.com>
2023-05-10 09:43:26 +0900

    ✨

--- matter/src/App.js
+++ matter/src/App.js
@@ -4,8 +4,6 @@ import {
   Route,
   BrowserRouter
 } from "react-router-dom";
-import IndexPage from './components/IndexPage';
-import Banner from './components/Banner';
 import Details from './pages/Details';
 import Products from './pages/Products';
 import Product from './pages/Product';
@@ -17,7 +15,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          {/* <Route path='/' element={<Banner></Banner>} /> */}
+          {/* <Route path='/Banner' element={<Banner></Banner>} /> */}
           <Route path='/' element={<Products />}/>
           <Route path="/detail/:id" element={<Details />} />
         </Routes>
--- matter/src/components/IndexPage.jsx
+++ matter/src/components/IndexPage.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './Banner'
 import Header from './Header'
-import Footer from './Footer'
 
 const IndexPage = () => {
   return (
     <div>
       <Banner></Banner>
       <Header></Header>
-      
     </div>
   )
 }
