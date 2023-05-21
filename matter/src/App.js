8090b55aa1fe1f2177b4c1a5eb5ccb707a18927f
jiin <jn_ll@naver.com>
2023-05-22 00:04:51 +0900

    🐛

--- matter/src/App.js
+++ matter/src/App.js
@@ -2,8 +2,12 @@ import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Details from "./pages/Details";
 import Products from "./pages/Products";
-
+import Product from "./pages/Product";
+import Detail from "./pages/Detail";
 import Index from "./components/Index";
+import { useEffect, useState } from "react";
+
+// import Index from './components/Index';
 // import Login from './pages/Login'
 
 function App() {
